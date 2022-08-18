760170f600f50e8f03f63885768812cfdab77a6c
Lucas Inojosa <95324295+LukeInojosa@users.noreply.github.com>
2022-08-18 10:44:59 -0300

    Versão final
    Nota máxima!! Felicidade!

--- kernel.asm
+++ kernel.asm
@@ -175,6 +175,16 @@ screen_clear:
     mov ax,13h
     int 10h
     ret
+
+clear_s:
+    mov ax,[videomem_addr]
+    mov es, ax             ; Beginning of VGA memory in segment 0xA000
+    mov ax, 0x00           ; Set the color to clear with 0x76 (green?) 0x00=black
+    xor di,di              ; Destination address set to 0
+    mov cx, (320*200)/2
+    rep stosd
+    ret
+
 clear_registers:
     xor ax,ax
     xor bx,bx
@@ -596,7 +606,7 @@ collision_exist:
 
 
     delay_fps
-    call screen_clear 
+    call clear_s       ; limpar screen a cada frame
     jmp menu 
  ret
 
@@ -672,7 +682,7 @@ loopGame:
     call collision
 
     delay_fps               ; delay de 1/30 segundos
-    call screen_clear       ; limpar screen a cada frame
+    call clear_s       ; limpar screen a cada frame
     jmp loopGame
 
     .end_game_loop:
